f9da9f8fc2c2c5d33d3df93d628467a36e5ac674
yuvromashov <yu.v.romashov@gmail.com>
2025-10-16 10:01:19 +0300

    Add files via upload

---
+++ main.f90
@@ -0,0 +1,76 @@
+
+program hello
+    use casta_scr
+    use casta_appl
+    implicit none
+
+    print *, "Hello World!"
+
+    call casta_scr_setfields(field2=25,field1=50,field3=20)
+    print*,casta_scr_opt
+    call casta_scr_setfields()
+    print*,casta_scr_opt
+    call casta_scr_setfields(field1=50)
+    print*,casta_scr_opt
+    call casta_scr_setfields(field2=25)
+    print*,casta_scr_opt
+    call casta_scr_setfields(field3=20)
+    print*,casta_scr_opt
+    call casta_scr_setfields(field1=5000,field2=250,field3=200)
+    print*,casta_scr_opt
+
+    call casta_scr_setfields()
+    call casta_scr_linestart()
+    call casta_scr_textwof(text='>',rep=casta_scr_opt%field1)
+    call casta_scr_textwof(text=' ',rep=1)
+    call casta_scr_textwof(text='>',rep=casta_scr_opt%field2)
+    call casta_scr_textwof(text=' ',rep=1)
+    call casta_scr_textwof(text='>',rep=casta_scr_opt%field3)
+    call casta_scr_linereturn()
+
+    call casta_scr_linestart()
+    call casta_scr_textwof(text='>',rep=casta_scr_opt%field1)
+    call casta_scr_textwof(text=' ',rep=1)
+    call casta_scr_textfar(text='text',field=casta_scr_opt%field2,blanc='>')
+    call casta_scr_textwof(text=' ',rep=1)
+    call casta_scr_textfal(text='text',field=casta_scr_opt%field3,blanc='>')
+    call casta_scr_linereturn()
+
+    call casta_scr_linesymbolfields(symb1='>',symb2='=',symb3='-')
+    call casta_scr_linesymbolfields(symb1='>')
+    call casta_scr_linesymbolfields(symb2='=')
+    call casta_scr_linesymbolfields(symb3='-')
+    call casta_scr_linesymbolfields(symb1='>',symb2='=')
+    call casta_scr_linesymbolfields(symb1='>',symb3='-')
+    call casta_scr_linesymbolfields(symb2='=',symb3='-')
+    call casta_scr_textblancfields(align1='r',align3='r',&
+    text1='text1',text2='text2',text3='text3',blanc1='>',blanc2='=',blanc3='-')
+    call casta_scr_textblancfields(align1='r',text1='text1',blanc2='>',blanc3='>')
+    call casta_scr_textblancfields(align2='r',text1='text1',blanc2='>',blanc3='>')
+    call casta_scr_textblancfields(align3='r',text1='text1',blanc2='>',blanc3='>')
+    call casta_scr_textblancfields(&
+    text1='text1',text2='text2',text3='text3',blanc1='>',blanc2='=',blanc3='-')
+
+    !call casta_scr_setfields(field1=0,field2=0,field3=0)
+    call casta_appl_entry(name='project',curdate='12.10.2025',curtime='16:23:35.123',&
+                          comline='d:/erte/esrgerreg/ergtretgert/project',&
+                          curwdir='d:/erte/esrgerreg/ergtretgert',&
+                          descr='it is testing for casta')
+    call casta_appl_message(mes1='mes1',mes2='mes2',mes3='mes3')
+    call casta_appl_message(mes1='mes1')
+    call casta_appl_message(mes2='mes2')
+    call casta_appl_message(mes3='mes3')
+    call casta_appl_message(mes1='mes1',mes2='mes2')
+    call casta_appl_message(mes1='mes1',mes3='mes3')
+    call casta_appl_message(mes2='mes2',mes3='mes3')
+    call casta_appl_exit(res='successful executed',curdate='12.10.2025',curtime='12:32:45.234',exetime='00:02:45.234')
+    call casta_appl_finalpause()
+
+
+
+
+
+
+
+end program
+
